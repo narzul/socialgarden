2c78fe9567723a4f384eb621f6caf00ff8e3d55b
Thomas <rohleder@protonmail.com>
2018-07-11 14:14:40 +0200

    test-api 2.7

--- python/TEST-API.sh
+++ python/TEST-API.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python2.7 terminal.py --wait -t getData python get-data-from-server-example.py
-python2.7 terminal.py --wait -t getAttribute python get-attributes-example.py
-python2.7 terminal.py --wait -t senddData python send-to-server-example.py
+python2.7 terminal.py --wait -t getData python2.7 get-data-from-server-example.py
+python2.7 terminal.py --wait -t getAttribute python2.7 get-attributes-example.py
+python2.7 terminal.py --wait -t senddData python2.7 send-to-server-example.py
