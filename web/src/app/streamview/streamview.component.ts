2a74ec9d4e969f36017478ea17566edcee4cb897
Thomas <rohleder@protonmail.com>
2018-08-16 10:19:54 +0200

    cleanup streamview.component.ts

--- web/src/app/streamview/streamview.component.ts
+++ web/src/app/streamview/streamview.component.ts
@@ -7,7 +7,6 @@ import { DatepickerOptions } from 'ng2-datepicker';
 import * as frLocale from 'date-fns/locale/fr';
 
 //// TODO: The strengh of Angular comes, in part from its strong use of components. This component could be split into smaller once, which would be benificial in regards to readablity.
-
 @Component({
   selector: 'app-streamview',
   templateUrl: './streamview.component.html',
@@ -28,7 +27,11 @@ export class StreamviewComponent implements OnInit {
   chart: Chart = []; // This will hold our chart meta info
   dataSet = []; // Holding data for drawing into graph
   streamLabels = [];
-
+  borderColors: any = [ //Array containing possible colors for line chart.
+    'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)',
+    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+  ];
+  chartConfig: any;
 
   //  tempStream: any;
   pipe = new DatePipe('en-EU'); //DatePipe for setting date format
@@ -49,52 +52,6 @@ export class StreamviewComponent implements OnInit {
     defaultOpen: false
   }
 
-  borderColors: any = [ //Array containing possible colors for line chart.
-    'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)',
-    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
-  ];
-  config: any = {
-    type: this.chartType,
-    data: {
-      labels: this.streamLabels,
-      datasets: this.dataSet
-    },
-
-    options: {
-      text: this.selectedColl,
-      events: ["mousemove", "mouseout", "click", "touchstart", "touchmove", "touchend"],
-      legend: {
-        display: true,
-        labels: {
-          fontFamily: 'Raleway',
-        }
-      },
-
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontFamily: 'Raleway',
-          }
-        }],
-        xAxes: [{
-          ticks: {
-            fontFamily: 'Raleway',
-            min: this.firstDate,
-            max: this.lastDate,
-          }
-        }]
-      },	// Container for pan options
-      tooltips: {
-        mode: 'point'
-      }, title: {
-        display: true,
-        fontFamily: 'Raleway',
-      },
-      animation: {
-        duration: 100
-      }
-    }
-  }
 
 
 
@@ -182,7 +139,7 @@ export class StreamviewComponent implements OnInit {
     this.listenerWrapperFunction = null;
     // Initiate Populate graph sequence step 5
     this.setConfig();
-    this.chart = new Chart('canvas', this.config);
+    this.chart = new Chart('canvas', this.chartConfig);
     // Initiate Populate graph sequence step 6
     this.listenerWrapperFunction = this.listenForNewData(value);
     this.listenToData = true;
@@ -191,7 +148,7 @@ export class StreamviewComponent implements OnInit {
   //Populate graph sequence step 5
   setConfig() {
     //// TODO: condence this. theres a lot of rendudancy
-    this.config = {
+    this.chartConfig = {
       type: this.chartType,
       data: {
         labels: this.streamLabels,
@@ -317,6 +274,6 @@ export class StreamviewComponent implements OnInit {
     this.getData(this.collections[0]);
   }
   ngAfterViewInit() {
-    this.chart = new Chart('canvas', this.config);
+    this.chart = new Chart('canvas', this.chartConfig);
   }
 }
